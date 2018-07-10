cf854d8bbc2d2e13b4629dad78b3f1973c299608
JBYoshi <jbyoshi@users.noreply.github.com>
2018-07-10 10:24:52 -0500

    Stub code for Bluetooth.

--- app/src/main/java/com/example/lenovo/huecontroller/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/huecontroller/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.lenovo.huecontroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  /**
+   * This method will be called (on the main thread for the app) when a
+   * Bluetooth message is received.
+   */
+  fun receiveMessage(message: String) {
+    // TODO
+  }
+
+  /**
+   * Call this method to send a Bluetooth message to the light.
+   */
+  fun sendMessage(message: String) {
+    // TODO
   }
 }
